5072fb42358d21e2a40cbe501ba1da5881e9d3ac
Woo Hyun <gon03004@gmail.com>
2023-04-27 23:08:40 +0900

    [Silver III] Title: 01타일, Time: 256 ms, Memory: 70224 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -1,8 +1,6 @@
 def fib(n):
     fib_list[0]=1
     fib_list[1]=1
-    if n <2:
-        return fib_list[n]
     
     for i in range(2,n+2):
         fib_list[i]= (fib_list[i-2]+fib_list[i-1]) % 15746
