164fe8bea0912725589ac88871fa9cac293f1b51
Woo Hyun <gon03004@gmail.com>
2023-04-28 15:34:17 +0900

    [Gold V] Title: 동전, Time: 84 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.py
@@ -0,0 +1,13 @@
+T = int(input())
+for _ in range(T):
+    n= int(input())
+    coin_list = map(int,input().split())
+    m=int(input())
+    dp =[0]*(m+1)
+    dp[0]=1
+    for coin in coin_list:
+        for i in range(1,m+1):
+            if i>=coin:
+                dp[i]+=dp[i-coin]
+                
+    print(dp[m])
