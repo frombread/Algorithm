6e245241912b40702e2c55a74ab9b1c567db5b39
Woo Hyun <gon03004@gmail.com>
2023-05-20 17:02:07 +0900

    [Silver IV] Title: 어두운 굴다리, Time: 96 ms, Memory: 44300 KB -BaekjoonHub

--- 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.py
+++ 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.py
@@ -6,9 +6,6 @@ m = int(input())
 n = int(input())
 
 street_lamp_list =list(map(int,input().split()))
-# street_lamp_list.append(0)
-# street_lamp_list.append(m)
-street_lamp_list.sort()
 result = []
 for i in range(n-1):
     result.append(math.ceil(abs(street_lamp_list[i]-street_lamp_list[i+1])/2))
@@ -17,3 +14,5 @@ result.append(abs(street_lamp_list[-1]-m))
 result.append(abs(0-street_lamp_list[0]))
 
 print(max(result))
+
+
