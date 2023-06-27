d70ffc0446e5a86094b26e2d85606ad9332b770d
Woo Hyun <gon03004@gmail.com>
2023-06-27 23:07:48 +0900

    [level 2] Title: 구명보트, Time: 15.06 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -0,0 +1,22 @@
+from collections import deque
+def solution(people, limit):
+    answer = 0
+    wei=0
+    q= deque( )
+    people.sort(reverse = 1)
+    for i in people:
+        q.append(i)
+    while q:
+        M_num = q.popleft()
+        wei += M_num
+        while wei < limit and q:
+            m_num = q.pop()
+            if wei + m_num >limit:
+                q.append(m_num)
+                break
+            else:
+                wei += m_num
+        wei =0
+        answer +=1
+        
+    return answer
