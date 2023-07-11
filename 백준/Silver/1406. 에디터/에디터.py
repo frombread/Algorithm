60495d2e04f693ebe47cb066941f88c18f672c53
Woo Hyun <gon03004@gmail.com>
2023-07-11 11:11:49 +0900

    [Silver II] Title: 에디터, Time: 460 ms, Memory: 179500 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.py
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -1,23 +1,21 @@
-m = input()
+m = list(input())
 n = int(input())
-commands = [input().split() for _ in range(n)]
-
-left_stack = list(m)
-right_stack = []
-
-for command in commands:
-    if command[0] == "L":
-        if left_stack:
-            right_stack.append(left_stack.pop())
-    elif command[0] == "D":
-        if right_stack:
-            left_stack.append(right_stack.pop())
-    elif command[0] == "B":
-        if left_stack:
-            left_stack.pop()
-    elif command[0] == "P":
-        left_stack.append(command[1])
-
-right_stack.reverse()
-result = left_stack + right_stack
-print("".join(result))
+list_ =[]
+# Cursor
+cursor = len(m)
+for _ in range(n):
+    a = input().split()
+    if a[0]=="L":
+        if len(m) > 0:
+            list_.append(m.pop())
+    elif a[0] == "D":
+        if len(list_) > 0:
+            m.append(list_.pop())
+    elif a[0] == "B":
+        if len(m)>0:
+            m.pop()
+    elif a[0] =="P":
+        m.append(a[1])
+list_.reverse()
+res = m+list_
+print("".join(res))
